a12eb2c4546694a9b75ee8562c818e107307cbb0
sschutzman <$t735815>
2017-12-11 10:13:16 -0600

    Updated exercise

---
+++ select_exercise.sql
@@ -0,0 +1,13 @@
+use codeup_test_db;
+
+SELECT name from albums where artist = 'Pink Floyd';
+
+SELECT release_date from albums where name = 'Sgt. Pepper''s Lonely Hearts Club Band';
+
+SELECT genre from albums where name = 'Nevermind';
+
+SELECT * from albums WHERE release_date = 1990;
+
+SELECT * FROM albums WHERE sales = 20;
+
+SELECT * from albums where genre = 'rock';
