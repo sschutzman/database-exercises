07af98fc0f41b3716c357835699b439ccd64b1a1
sschutzman <$t735815>
2017-12-12 14:18:20 -0600

    completed exercises

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -1,14 +1,17 @@
-use employees_db;
-
 SELECT emp_no, first_name
 FROM employees
-WHERE first_name IN('Irena', 'Vidya', 'Maya');
+WHERE first_name IN('Irena', 'Vidya', 'Maya')
+Order by first_name ASC;
+
+SELECT first_name, last_name
+FROM employees
+WHERE first_name IN('Irena', 'Vidya', 'Maya')
+Order by first_name DESC, last_name desc;
 
 
 SELECT last_name
 FROM employees
-WHERE last_name LIKE 'E%';
-
+WHERE last_name LIKE 'E%' ORDER BY emp_no DESC;
 SELECT *
 FROM employees
 WHERE hire_date >= '1990' AND hire_date <= '1999';
@@ -35,7 +38,8 @@ WHERE last_name LIKE 'E%' and  last_name LIKE '%E';
 
 Select *
 from employees
-Where hire_date like '199%' and birth_date like '%12-25';
+Where hire_date like '199%' and birth_date like '%12-25'
+ORDER BY birth_date ASC, hire_date DESC;
 
 SELECT *
 FROM employees
